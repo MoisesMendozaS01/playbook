a20b30cf03404dfb2147054d3f0023202c3a38da
= <=>
2022-04-12 16:44:07 -0500

    Agregando ejemplo 2 de Examples1

---
+++ weekly_mission_2/examples_1/examples1_2.js
@@ -0,0 +1,6 @@
+// Ejemplo 2: for Each para calcular la suma de todos los elementos de una lista
+let sum = 0;
+const numbers2 = [10, 20, 30, 40, 50];
+numbers2.forEach(num => sum += num)
+console.log("Ejemplo 2: Cálculo de la suma de los elementos de la lista")
+console.log(sum)
