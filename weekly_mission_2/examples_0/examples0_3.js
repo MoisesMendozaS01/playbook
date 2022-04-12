31ceb7e3a41f2ad541ac24fd82c989d17ce67d69
= <=>
2022-04-12 16:34:30 -0500

    Agregando ejemplo 3 de Examples0

---
+++ weekly_mission_2/examples_0/examples0_3.js
@@ -0,0 +1,19 @@
+// Ejemplo 3: Objeto con diferentes propiedades
+const myObject3 = {
+    name: "Fred",
+    age: 2,
+    nicknames: [
+     "Frederick",
+      "Frederick Máximo Meridio",
+      "Fredwart"
+    ],
+    address: {
+      zip_code: "10000",
+      street: "Morelos AV",
+      city: "Morelia"
+    }
+  }
+  console.log("Ejemplo 3: Objeto con diferentes propiedades")
+  console.log(myObject3)
+  console.log(myObject3.name)
+  console.log(myObject3["address"])
