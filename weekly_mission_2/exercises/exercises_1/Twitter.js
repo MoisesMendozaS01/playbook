afd24374daab4cd6c80e1d3e3829b5a9fbd2081d
= <=>
2022-04-13 18:49:52 -0500

    Agragando modelar objeto sobre Twitter ejercicio 1

---
+++ weekly_mission_2/exercises/exercises_1/Twitter.js
@@ -0,0 +1,24 @@
+const twitter={
+    user:{
+        user:"Ruben",
+        username:"El Rubius",
+        bio:"streamer......",
+        age:31,
+    },
+    trendingtopic:{
+        redid:"/replace",
+        twitter:{
+            Donaciones:["Fundación Altarriba",
+                "Hocicos y Huellas",
+                "Protectora D'Animals Mataro",
+                "Fund. Hablemos por Ellos",
+                "Con B de Galgo",
+                "GOS-SOS",
+                "Protectora Granollers",
+                "Puntanimals"]
+        }
+    },
+    hahstag:["#OurMne","#rubius-fanart","#karmaland","#minecraft"]
+}
+console.log(twitter.trendingtopic.twitter)
+    
