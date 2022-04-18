a14d3416776e5af94d296cd1ea68f5e7bcbdfe84
= <=>
2022-04-17 21:34:29 -0500

    Agregando ejercicio 1 modulacion de facebook

---
+++ weekly_mission_2/exercises/exercises_1/Facebook.js
@@ -0,0 +1,38 @@
+const Facebook={
+    User:{
+        name:"Moises Mendoza",
+        perfil_picture:"/imageperfil",
+        coverpage_picture:"/imagecover",
+        age:24,
+        friends:500,
+    },
+    Post:{
+        post_1:{
+            tilte:"firts post",
+            date:"##/##/####",
+            content:{
+                text:"Hi, this is my first post",
+                image:"/image",
+                friends_tags:["juanito","Paco"],
+                link:"link.org"
+            }
+        },
+        post_2:{
+            tilte:"second post",
+            date:"##/##/####",
+            content:{
+                text:"Hi, this is my second post",
+                image:"/image",
+                friends_tags:["juanito","Paco"],
+                link:"link.org"
+            }}
+    },
+    Biography:{
+        country:"México",
+        work:"hola.inc",
+        emotional_situation:"single",
+        School:"SchoolNew",
+        gender:"male",
+        city:"Monterrey"
+    }
+}
