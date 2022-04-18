92bebd175238eea6418c1cfa4ba32b47d37bae30
= <=>
2022-04-17 22:10:33 -0500

    agregando ejercicio 1 modelacion de objeto Uber

---
+++ weekly_mission_2/exercises/exercises_1/Uber.js
@@ -0,0 +1,35 @@
+const Uber={
+    profile:{
+        name:"Moises",
+        perfil_image:"/image",
+        travels:1000,
+        qualification:4.70,
+        months:5,
+        languages:["Spanish","English"],
+        city:"Monterrey",
+        appreciation:["Hero","Kind","Excellent service"],
+        comments:{
+            user_one:{
+                coment:"excellent service",
+                stars:5
+            },
+            user_two:{
+                coment:"excellent service",
+                stars:4.5
+            }
+        }
+    },
+    travel:{
+        user:"Pablito123",
+        user_image:"/image",
+        cel:"########",
+        arrave_time:30,
+        travel_time:30,
+        direction:"centrer street",
+        payment:"cash",
+        options:{
+            travel:["cancel","report","over"]
+        }
+    }
+}
+console.log(Uber)
