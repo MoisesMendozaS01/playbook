a35f42a70d3ad655973c73114d8ebbdcbd955243
= <=>
2022-04-18 22:02:49 -0500

    agregando ejercicio 3, creando clase de twitter

---
+++ weekly_mission_2/exercises/exercises_3/twiter.js
@@ -0,0 +1,14 @@
+class twiter{
+    constructor(user,username,bio,age,redid,twiter,hahstag){
+        this.user=user,
+        this.username=username
+        this.bio=bio
+        this.age=age
+        this.redid=redid
+        this.twiter=twiter
+        this.hahstag=hahstag
+    }
+}
+
+const the_twitter=new twiter("Ruben","ElRubiusOMG","streamer...",31,"/Replce",["Fundación Altarriba","Hocicos y Huellas", "Protectora D'Animals Mataro","Fund. Hablemos por Ellos"],["#OurMne","#rubius-fanart","#karmaland","#minecraft"])
+console.log(the_twitter)
