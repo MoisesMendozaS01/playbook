6bb558ae500ed5b558441dac8a59e660951b23fc
= <=>
2022-04-25 22:19:10 -0500

    Ejemplo semana 2 dia 3 objeto instagram

---
+++ weekly_mission_2/exercises/exercises_1/instagram.js
@@ -0,0 +1,46 @@
+//// ¿Que veo en intagram?
+/*
+Perfil del usuario
+    nombre
+    usuario
+    seguidores
+    seguidos
+    numeros de post
+    bio
+fotos de usuario
+    imagen
+    like
+    fecha de creacion
+    descripcion
+    usuario
+mensajes
+    usuario destinatario
+    listas de mensajes
+videos del usuario
+    imagen
+    like
+    fecha de creacion
+    descripcion
+    usuario
+reel del usuario
+    imagen
+    like
+    fecha de creacion
+    descripcion
+    usuario
+fotos etiquetadas
+    imagen
+    like
+    fecha de creacion
+    descripcion
+    usuario que etiqueta
+    usuario
+*/
+
+const ig_user={
+    name:"username",
+    username:"Username",
+    followers:200,
+    following:50,
+    total_post:40
+}
